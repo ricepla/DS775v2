90d012c1d2cc16952a223ef327b1e942b3936e6e
Jeff Baggett <baggett.jeff@uwlax.edu>
2021-01-24 21:34:59 -0600

    Spring 2021 Updates

--- Lessons/Project 02 Presentation/tpot_optimal_pipeline.py
+++ Lessons/Project 02 Presentation/tpot_optimal_pipeline.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import FastICA
-from sklearn.linear_model import RidgeCV, SGDRegressor
+from sklearn.linear_model import LassoLarsCV, RidgeCV
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.pipeline import make_pipeline, make_union
-from tpot.builtins import StackingEstimator
+from sklearn.preprocessing import PolynomialFeatures
+from tpot.builtins import StackingEstimator, ZeroCount
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -14,11 +13,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=8675309)
 
-# Average CV score on the training set was: 0.47137076042557907
+# Average CV score on the training set was: 0.46738885050582135
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=SGDRegressor(alpha=0.0, eta0=1.0, fit_intercept=True, l1_ratio=0.75, learning_rate="constant", loss="huber", penalty="elasticnet", power_t=1.0)),
-    StackingEstimator(estimator=KNeighborsRegressor(n_neighbors=32, p=1, weights="uniform")),
-    FastICA(tol=0.9),
+    ZeroCount(),
+    StackingEstimator(estimator=LassoLarsCV(normalize=True)),
+    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
     RidgeCV()
 )
 # Fix random state for all the steps in exported pipeline
